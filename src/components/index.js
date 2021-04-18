01971c3d436757b589ab6f5d31207b3e078bf9dc
Sebatian <sebasmayorga7@gmail.com>
2021-04-18 14:23:49 -0700

    Added experience

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,5 @@
 export { default as CVMenu } from "./Menu/Menu";
 
 export { default as Sidebar }  from "./Sidebar/Sidebar";
+
+export { default as ExperienceItem } from "./ExperienceItem/ExperienceItem";
